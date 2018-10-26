2cefec1891f39816f381373b9d3ee3e76e6cf06d
slawek <slawomirpawlowski@gmail.com>
2018-10-27 00:50:02 +0200

    Added user session with flask_login

--- plants/auth/routes.py
+++ plants/auth/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
 
 from plants import db, bcrypt
 from plants.auth import auth
 from plants.auth.models import User
 from plants.auth.forms import RegistrationForm, LoginForm
+
 
 @auth.route('/')
 def landing():
@@ -11,6 +13,9 @@ def landing():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.landing'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -24,12 +29,30 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.landing'))
+
     form = LoginForm()
+    print(request.args.get('next'))
+
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            flash(f'Logowanie udane', 'success')
-            return redirect(url_for('auth.landing'))
+            login_user(user, remember=form.stay_loggedin.data)
+            next_page = request.args.get('next')
+            print(next_page)
+            return redirect(next_page) if next_page else redirect(url_for('auth.landing'))
         else:
             flash(f'Zły email lub hasło!', 'success')
     return render_template('login.html', title='login', form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.landing'))
+
+
+@auth.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='account')
