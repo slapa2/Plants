6f69cece6b94830c6796c6a0720df4e7d07adc98
slawek <slawomirpawlowski@gmail.com>
2018-10-30 23:10:30 +0100

    #6, #7

--- plants/plants/forms.py
+++ plants/plants/forms.py
@@ -1,11 +1,24 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, ValidationError
 
 from plants.plants.models import Plant
 
 class AddPlantForm(FlaskForm):
     polish_name = StringField('Nazwa:', validators=[DataRequired()])
+    latin_name = StringField('Nazwa łacińska:', validators=[])
+
+    light_level = StringField('Światła:', validators=[])
+    light_description = IntegerField('Poziom śqiatła 0-5:', validators=[])
+    water_level = IntegerField('Ilość wody:', validators=[])
+    water_description = StringField('Podlewanie:', validators=[])
+    temperature = StringField('Temperatura:', validators=[])
+    soil = StringField('Gleba:', validators=[])
+    fertilizing = StringField('Nawożenie:', validators=[])
+    transplanting = StringField('Przesadzanie:', validators=[])
+    multiplication = StringField('Rozmnażanie:', validators=[])
+
     image = StringField('url obrazka:', validators=[DataRequired()])
+
     submit = SubmitField("Dodaj")
--- plants/plants/routes.py
+++ plants/plants/routes.py
@@ -17,9 +17,67 @@ def plants_catalog():
 def add_plant():
     form = AddPlantForm()
     if form.validate_on_submit():
-        plant = Plant(polish_name=form.polish_name.data, image=form.image.data)
+        plant = Plant(
+            polish_name=        form.polish_name.data,
+            latin_name=         form.latin_name.data,
+            light_level =       form.light_level.data,
+            light_description = form.light_description.data,
+            water_level =       form.water_level.data,
+            water_description = form.water_description.data,
+            temperature =       form.temperature.data,
+            soil =              form.soil.data,
+            fertilizing =       form.fertilizing.data,
+            transplanting =     form.transplanting.data,
+            multiplication =    form.multiplication.data,
+            image=form.image.data)
+
         db.session.add(plant)
         db.session.commit()
         flash(f'Roślina {form.polish_name.data} została dodana do bazy!', 'success')
         return redirect(url_for('plants.plants_catalog'))
     return render_template('addPlant.html', form=form, title='nowa roślina')
+
+@plants.route('/edit.plant/<plant_id>', methods=['GET', 'POST'])
+@login_required
+def edit_plant(plant_id):
+
+    plant = Plant.query.get(plant_id)
+    form = AddPlantForm()
+
+    if form.validate_on_submit():
+        plant.polish_name=form.polish_name.data
+        plant.latin_name=form.latin_name.data
+        plant.light_level=form.light_level.data
+        plant.light_description=form.light_description.data
+        plant.water_level=form.water_level.data
+        plant.water_description=form.water_description.data
+        plant.temperature=form.temperature.data
+        plant.soil=form.soil.data
+        plant.fertilizing=form.fertilizing.data
+        plant.transplanting=form.transplanting.data
+        plant.multiplication=form.multiplication.data
+        plant.image=form.image.data
+        db.session.add(plant)
+        db.session.commit()
+        flash(f'Roślina {form.polish_name.data} została dodana do bazy!', 'success')
+        return redirect(url_for('plants.plants_catalog'))
+
+    form.polish_name.data = plant.polish_name
+    form.latin_name.data = plant.latin_name
+    form.light_level.data = plant.light_level
+    form.light_description.data = plant.light_description
+    form.water_level.data = plant.water_level
+    form.water_description.data = plant.water_description
+    form.temperature.data = plant.temperature
+    form.soil.data = plant.soil
+    form.fertilizing.data = plant.fertilizing
+    form.transplanting.data = plant.transplanting
+    form.multiplication.data = plant.multiplication
+    form.image.data = plant.image
+
+    return render_template('addPlant.html', form=form, title='nowa roślina')
+
+@plants.route('/plants/<plant_id>')
+def plant_info(plant_id):
+    plant = Plant.query.get(plant_id)
+    return render_template('plant.html', title='plant info', plant=plant)
