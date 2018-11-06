4b021e40239cdfc8dc2f6f67a1cc497622717e7e
slawek <slawomirpawlowski@gmail.com>
2018-11-06 21:17:54 +0100

    run.py file prod configuration

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from plants import create_app, db
 from plants.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(email='user@test.pl').first():
-            User.creata_user('user@test.pl', 'user')
 
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(email='user@test.pl').first():
+        User.creata_user('user@test.pl', 'user')
     flask_app.run()
