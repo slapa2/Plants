7dea446a132095f4c49fc431867708dfe49f019f
slawek <slawomirpawlowski@gmail.com>
2018-11-06 22:51:05 +0100

    run.py file chenged

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from plants import create_app, db
 from plants.auth.models import User
+from sqlalchemy import exc
 
 
 flask_app = create_app('prod')
 with flask_app.app_context():
+
     db.create_all()
-    if not User.query.filter_by(email='user@test.pl').first():
-        User.creata_user('user@test.pl', 'user')
-    flask_app.run()
+    try:
+        if not User.query.filter_by(email='user@test.pl').first():
+            User.creata_user('user@test.pl', 'user')
+    except exc.IntegrityError:
+        flask_app.run(host='0.0.0.0', port=80)
