b9d5a6916098500be2028ec939e0c74ed130b3ec
slawek <slawomirpawlowski@gmail.com>
2018-11-07 23:14:41 +0100

    ryn.py fixed to prod version

--- run.py
+++ run.py
@@ -2,13 +2,11 @@ from plants import create_app, db
 from plants.auth.models import User
 from sqlalchemy import exc
 
-
-if __name__ == '__main__':
-    flask_app = create_app('dev')
-    with flask_app.app_context():
-
-        db.create_all()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    try:
         if not User.query.filter_by(email='user@test.pl').first():
             User.creata_user('user@test.pl', 'user')
-
-        flask_app.run()
+    except exc.IntegrityError:
+        flask_app.run(host='0.0.0.0', port=80)
