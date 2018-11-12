b6794107fce36df7e577363eec95d15af44f3cc6
Slawomir Pawlowski <slawomirpawlowski@gmail.com>
2018-11-12 18:36:42 +0100

    tu działało

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
-from plants import create_app
+from plants import create_app, db
+from plants.auth.models import User
+from sqlalchemy import exc
 
 flask_app = create_app()
 with flask_app.app_context():
-    flask_app.run()
+    db.create_all()
+    try:
+        if not User.query.filter_by(email='user@test.pl').first():
+            User.creata_user('user@test.pl', 'user')
+    except exc.IntegrityError:
+        flask_app.run()
