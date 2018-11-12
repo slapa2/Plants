9bb48a2b0845738add243495364a74ebc29da7bd
Slawomir Pawlowski <slawomirpawlowski@gmail.com>
2018-11-12 18:22:19 +0100

    set prod debug config True

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     if os.environ.get('ENV') == 'prod':
-        DEBUG = False
+        DEBUG = True
         SECRET_KEY = os.environ['SECRET_KEY']
         SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
         SQLALCHEMY_TRACK_MODIFICATIONS = False
