8c7f11041b2b9502519d9868fc19af8d819ba18a
Slawomir Pawlowski <slawomirpawlowski@gmail.com>
2018-11-12 17:50:03 +0100

    some code clean at start test

--- plants/__init__.py
+++ plants/__init__.py
@@ -8,6 +8,9 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
 
 def create_app(config_type): # dev, test, prod
     app = Flask(__name__)
@@ -16,14 +19,10 @@ def create_app(config_type): # dev, test, prod
 
     db.init_app(app)
     bcrypt.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    login_manager.login_message_category = 'info'
 
     from plants.auth import auth
-    app.register_blueprint(auth)
-
     from plants.plants import plants
+    app.register_blueprint(auth)
     app.register_blueprint(plants)
 
     return app
