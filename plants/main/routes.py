e839250bc79a144d9c6469bca1e405a33e77f6d9
Slawomir Pawlowski <slawomirpawlowski@gmail.com>
2018-11-15 00:07:19 +0100

    Edited /contact rout to send email

--- plants/main/routes.py
+++ plants/main/routes.py
@@ -1,11 +1,23 @@
 from flask import render_template
+from flask_mail import Message
+from plants import mail
 from plants.main import main
+from plants.main.forms import ContactForm
 
 
 @main.route('/')
 def landing():
     return render_template('landing.html')
 
-@main.route('/contact')
+@main.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html', title='kontakt')
+    form = ContactForm()
+    if form.validate_on_submit():
+    	email = form.email.data
+    	question = form.question.data
+
+    	msg = Message('[MyPlants] - Pytanie', sender=email, recipients=['bazaroslin@gmail.com'])
+    	msg.body = question
+
+    	mail.send(msg)
+    return render_template('contact.html', title='Kontakt', form=form)
