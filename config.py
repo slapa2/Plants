c2e329ac29d8980688a5e554637a05bb43f55cdc
Slawomir Pawlowski <slawomirpawlowski@gmail.com>
2018-11-21 23:25:01 +0100

    added local db config to os env

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
+
 class Config():
     DEBUG = True
     SECRET_KEY = 'd9c00b86f36f99cbd1f140b464bb36f1'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -17,5 +18,4 @@ class Config():
     if os.environ.get('ENV') == 'prod':
         DEBUG = False
         SECRET_KEY = os.environ['SECRET_KEY']
-        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
         SQLALCHEMY_TRACK_MODIFICATIONS = False
